ace876b8702ad76aa2c29f652114fac91be4d368
chinyishan <star138020@gmail.com>
2023-07-27 23:08:40 +0800

    拆分 component

---
+++ src/components/maskMap.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mask-map" id="mask-map"></div>
+</template>
+
+<script setup>
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/reset.scss'
+// import './assets/style.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
