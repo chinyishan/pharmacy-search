012f245e8b19f78f5fb4b42968bf5f990de73372
chinyishan <star138020@gmail.com>
2023-08-09 23:29:02 +0800

    串接燈箱詳細資料

--- src/components/lightBox.vue
+++ src/components/lightBox.vue
@@ -2,9 +2,8 @@
   <transition name="model">
     <div class="modal-mask" v-show="showModal">
       <div class="modal-wrapper" @click.self="close">
-        
         <div class="modal-container">
-          <div class="modal-body">
+          <div class="modal-body" v-if="currStore">
             <h1 class="store-name">{{ currStore?.name }}</h1>
             <hr>
             <h2 class="title">營業時間</h2>
@@ -24,33 +23,15 @@
               <tbody>
                 <tr>
                   <th>早上</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td v-for="(value, key) in servicePeriods[0]" :key="key">{{ value }}</td>
                 </tr>
                 <tr>
                   <th>中午</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td v-for="(value, key) in servicePeriods[1]" :key="key">{{ value }}</td>
                 </tr>
                 <tr>
                   <th>晚上</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td v-for="(value, key) in servicePeriods[2]" :key="key">{{ value }}</td>
                 </tr>
               </tbody>
             </table>
@@ -95,9 +76,18 @@ const currStore = computed(() => {
   return store.state.stores.filter((i) => i.id === infoBoxSid.value)[0]
 })
 
-const servicePeriods = () => {
+//營業時間
+const servicePeriods = computed(() => {
+  console.log(currStore?.value);
+  let servicePeriods = currStore.value['service_periods'] || '';
+  servicePeriods = servicePeriods.replace(/N/g, 'O').replace(/Y/g, 'X');
 
-}
+  return servicePeriods ? [
+    servicePeriods.slice(0, 7).split(''),
+    servicePeriods.slice(7, 14).split(''),
+    servicePeriods.slice(14, 21).split('')
+  ] : servicePeriods;
+})
 
 </script>
 
@@ -112,66 +102,53 @@ const servicePeriods = () => {
   background-color: rgba(0, 0, 0, .6);
   display: table;
   transition: opacity .3s ease;
-}
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-  cursor: pointer;
-}
-.modal-container {
-  width: 520px;
-  margin: 0px auto;
-  padding: 10px 30px;
-  background-color: #fff;
-  border-radius: 5px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .3);
-  transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-.modal-body {
-  color: #42b983;
-  margin: 20px 0;
+
+  .modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+    cursor: pointer;
+
+    .modal-container {
+      width: min(520px, 90%);
+      margin: 0px auto;
+      padding: min(30px, 5%);
+      background-color: #fff;
+      border-radius: 5px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, .3);
+      transition: all .3s ease;
+      font-family: Helvetica, Arial, sans-serif;
+      .modal-body {
+        color: #343434;
+        .store-name {
+          font-size: 1.6rem;
+          font-weight: bold;
+          line-height: 1.5;
+          color: #1a1a1a;
+        }
+        .title{
+          font-weight: 500;
+          margin-bottom: .5rem;
+          line-height: 1.7;
+        }
+        table {
+          border-spacing: 0;
+          border-radius: 3px;
+          width: 100%;
+          margin-bottom: 1rem;
+        }
+        th{
+          background-color: #3b54d4;
+          color: #fff;
+          font-weight: 900;
+        }
+        td, th{
+          padding: .3em;
+          text-align: center;
+          line-height: 1.5rem;
+        }
+      }
+    }
+  }
 }
 </style>
 
-<style>
-.modal-enter {
-  opacity: 0;
-}
-.modal-leave-active {
-  opacity: 0;
-}
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  transform: scale(1.1);
-}
-</style>
-
-<style lang="scss" scoped>
-.store-name {
-  font-size: 1.6rem;
-  font-weight: bold;
-  line-height: 1.5;
-}
-.title{
-  font-weight: 500;
-  margin-bottom: .5rem;
-  line-height: 1.7;
-}
-table {
-  border-spacing: 0;
-  border-radius: 3px;
-  width: 100%;
-  margin-bottom: 1rem;
-}
-th{
-  background-color: #42b983;
-  color: #fff;
-  font-weight: 900;
-}
-td, th{
-  padding: .3em;
-  text-align: center;
-  line-height: 1.5rem;
-}
-</style>
