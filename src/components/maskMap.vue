ac6ea6b174d0afb703e25514fd66a1f5eec44571
chinyishan <star138020@gmail.com>
2025-04-29 21:59:38 +0800

    修正路徑BUG

--- src/components/maskMap.vue
+++ src/components/maskMap.vue
@@ -3,22 +3,32 @@
 </template>
 
 <script setup>
-import { useStore } from 'vuex'
-import { ref, onMounted, computed, watch, defineExpose, defineProps } from 'vue';
-import L from 'leaflet';
+import { useStore } from "vuex";
+import {
+  ref,
+  onMounted,
+  computed,
+  watch,
+  defineExpose,
+  defineProps,
+} from "vue";
+import L from "leaflet";
+import iconUrlImg from "@/assets/images/geo-alt-fill-2.png";
+import shadowUrlImg from "@/assets/images/geo-shadow.png";
 
 const store = useStore();
-const map  = ref({});
+const map = ref({});
 const markers = ref([]);
 
 onMounted(() => {
-	map.value = L.map( 'mask-map' , {
-		center: [25.03, 121.55],
-		zoom: 14,
-	});
-	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		maxZoom: 18,
+  map.value = L.map("mask-map", {
+    center: [25.03, 121.55],
+    zoom: 14,
+  });
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18,
   }).addTo(map.value);
 });
 
@@ -26,79 +36,78 @@ const currDistrictInfo = computed(() => store.getters.currDistrictInfo);
 const filteredStores = computed(() => store.getters.filteredStores);
 
 watch(currDistrictInfo, (dis) => {
-	// 切換行政區地圖中心點
-	map.value.panTo(new L.LatLng(dis.latitude, dis.longitude))
+  // 切換行政區地圖中心點
+  map.value.panTo(new L.LatLng(dis.latitude, dis.longitude));
 });
 watch(filteredStores, (stores) => {
-	// 先清除原有 marker
-	clearMarkers()
-	// 藥局資訊加上對應 marker
-	stores.forEach((el) => addMarker(el))
-})
+  // 先清除原有 marker
+  clearMarkers();
+  // 藥局資訊加上對應 marker
+  stores.forEach((el) => addMarker(el));
+});
 
 const addMarker = (item) => {
-	// 標記圖示
-	const ICON = L.icon({
-		iconUrl: '/src/assets/images/geo-alt-fill-2.png',
-		shadowUrl: '/src/assets/images/geo-shadow.png',
-    iconSize:     [40, 40],
-    shadowSize:   [40, 40],
-    iconAnchor:   [22, 94],
-    shadowAnchor: [10, 90], 
-    popupAnchor:  [-3, -90]
-	})
+  // 標記圖示
+  const ICON = L.icon({
+    iconUrl: iconUrlImg,
+    shadowUrl: shadowUrlImg,
+    iconSize: [40, 40],
+    shadowSize: [40, 40],
+    iconAnchor: [22, 94],
+    shadowAnchor: [10, 90],
+    popupAnchor: [-3, -90],
+  });
 
-	// 將標記放置地圖上
-	const marker = L.marker([item.longitude, item.latitude], {icon: ICON})
-	.addTo(map.value)
-	.bindPopup(`<h2 class="popup-name">${item.name}</h2>`)
-	.openPopup()
+  // 將標記放置地圖上
+  const marker = L.marker([item.longitude, item.latitude], { icon: ICON })
+    .addTo(map.value)
+    .bindPopup(`<h2 class="popup-name">${item.name}</h2>`)
+    .openPopup();
 
-	// 往 marker 加入 id 經緯度
-	marker.markerId = item.id
-	marker.lng = item.longitude
-	marker.lat = item.latitude
+  // 往 marker 加入 id 經緯度
+  marker.markerId = item.id;
+  marker.lng = item.longitude;
+  marker.lat = item.latitude;
 
-	markers.value.push(marker)
-}
+  markers.value.push(marker);
+};
 
 // 清除地圖標記
 const clearMarkers = () => {
-	map.value.eachLayer((layer) => {
-		if(layer instanceof L.Marker) {
-			map.value.removeLayer(layer)
-		}
-	})
-	//清空陣列
-	markers.value.length = 0
-}
+  map.value.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      map.value.removeLayer(layer);
+    }
+  });
+  //清空陣列
+  markers.value.length = 0;
+};
 // const props = defineProps({
 // 	triggerPopup: Function
 // })
 
 // null 沒有使用到 triggerPopup
 const triggerPopup = (markerId) => {
-	// 找出目標藥局
-	const marker = markers.value.find((i) => i.markerId === markerId);
-	// 地圖中心指向目標，開啟popup
-	map.value.flyTo(new L.LatLng(marker.lng, marker.lat), 15);
-	marker.openPopup();
-}
+  // 找出目標藥局
+  const marker = markers.value.find((i) => i.markerId === markerId);
+  // 地圖中心指向目標，開啟popup
+  map.value.flyTo(new L.LatLng(marker.lng, marker.lat), 15);
+  marker.openPopup();
+};
 
 defineExpose({
-	triggerPopup
-})
-
+  triggerPopup,
+});
 </script>
 
 <style lang="scss" scoped>
 .mask-map {
-	position: relative;
-		width: 100%;
-		height: 100%;
-    top: 0;
-    right: 0%;
-    background-color: #aaa;
-    z-index: 5;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  right: 0%;
+  background-color: #aaa;
+  z-index: 5;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/pharmacy-search/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // 設定 @ 指向 src
+    },
+  },
   // vite IP 配置
   server: {
     host: "0.0.0.0",
